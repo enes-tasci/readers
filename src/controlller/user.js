72db74c08a26fd94b80ab0d50582488a26a99fbb
Enes Kaya <enestasci1180@gmail.com>
2025-04-24 09:42:00 +0300

    Profil sayfası eklendi.

--- src/controlller/user.js
+++ src/controlller/user.js
@@ -1,3 +1,15 @@
+const User = require("../models/user");
+
+exports.profile_get = async (req,res) => {
+    const username = req.session.username;
+
+    const user = await User.findOne();
+    
+    res.render("user/profile",{
+        user: user
+    });
+};
+
 exports.home_get = (req,res)=>{
     res.render("user/index");
 };
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,8 +4,11 @@ const router = express();
 
 // IMPORT FILES
 const userController = require("../controlller/user");
+const auth = require("../middleware/authControl");
 
 // PAGES
+router.get("/profil", auth.isNotAuth, userController.profile_get);
+
 router.get("/", userController.home_get);
 
 module.exports = router;
